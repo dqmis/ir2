0e905616b238019cc8e3fb82152fa465dc807110
dqmis <dom.seputis@gmail.com>
2024-11-09 12:35:41 +0100

    Adding initial refactoring

---
+++ tests/integration/test_inference_model.py
@@ -0,0 +1,18 @@
+from src.inference_model import Vec2textInferenceModel
+
+if __name__ == "__main__":
+    model = Vec2textInferenceModel(
+        model_name="bert-base-uncased", corrector_name="bert-base-uncased"
+    )
+
+    INPUT = (
+        [
+            "Jack Morris is a PhD student at Cornell Tech in New York City",
+            "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity",  # noqa
+        ],
+    )
+
+    embeddings = model.get_embeddings(INPUT)
+    inverted_embeddings = model.invert_embeddings(embeddings, num_steps=10)
+
+    print(inverted_embeddings)
