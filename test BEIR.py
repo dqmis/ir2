29194a9537ffe1fbbc2fc1a7514de17e33b15848
Plutorian-Programmer <karsten.langerak@gmail.com>
2024-11-14 09:51:24 +0100

    Update test BEIR.py

--- test BEIR.py
+++ test BEIR.py
@@ -2,10 +2,14 @@
 import pathlib, os
 from beir import util, LoggingHandler
 from beir.datasets.data_loader import GenericDataLoader
+import sklearn.metrics
 from transformers import AutoModel, AutoTokenizer, PreTrainedTokenizer, PreTrainedModel
 import torch
 import vec2text
 from alive_progress import alive_bar
+import sklearn
+import numpy as np
+import math
 def get_gtr_embeddings(text_list,
                        encoder: PreTrainedModel,
                        tokenizer: PreTrainedTokenizer) -> torch.Tensor:
@@ -40,7 +44,24 @@ def calc_cosine_score(query_embeddings, corpus_embeddings):
     cosine_sim = dot / torch.outer(query_norm, corpus_norm)
     
     return cosine_sim
-        
+
+def calc_NDCG(score_tensor, corpus_ids, query_ids, qrels):
+    ranking = torch.argsort(score_tensor)
+    normelizer = np.arange(2, 12)
+    normelizer = np.log2(normelizer)
+    NDCG = 0
+    for query_id in query_ids:
+        print(qrels[query_ids[0]]) 
+        relevant_docs = qrels[query_id]
+        ideal_score = np.array(sorted(list(relevant_docs.values()))[::-1][:10])
+        query_ranking = ranking[query_id][:10]
+        pred_score = np.array([relevant_docs[corpus_ids[doc_id]] for doc_id in query_ranking])
+        DCG = np.sum(pred_score / normelizer)
+        IDCG = np.sum(ideal_score / normelizer)
+        NDCG += DCG / IDCG
+    NDCG /= len(query_ids)
+    return NDCG
+          
 def BEIR_loop(dataset = "scifact", corpus_batch_size = 64):
     corpus, queries, qrels = load_dataset(dataset)
     encoder = AutoModel.from_pretrained("sentence-transformers/gtr-t5-base").encoder.to("cuda")
@@ -51,7 +72,6 @@ def BEIR_loop(dataset = "scifact", corpus_batch_size = 64):
     total_batches = len(corpus_ids) // corpus_batch_size
     score_tensor = torch.zeros((len(query_ids), len(corpus_ids)))
     
-    
     with alive_bar(total_batches) as bar:
         for i in range(total_batches):
             batch = corpus_ids[i*corpus_batch_size : (i+1) * corpus_batch_size]
@@ -61,7 +81,8 @@ def BEIR_loop(dataset = "scifact", corpus_batch_size = 64):
             score_tensor[:, i*corpus_batch_size : (i+1) * corpus_batch_size] = cosine_sim
             bar()
     # query_embeddings = embed_data()
-    pass
+    NDCG = calc_NDCG(score_tensor, corpus_ids, query_ids, qrels)
+    
 
 # corpus, queries, qrels = load_dataset()
 # print(len(queries))
