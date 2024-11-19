a3974a879a0df4352b2c46849e2522092647200e
Plutorian-Programmer <karsten.langerak@gmail.com>
2024-11-19 15:40:21 +0100

    Update inversion_attacks.py

--- scripts/inversion_attacks.py
+++ scripts/inversion_attacks.py
@@ -17,6 +17,7 @@ from ir2.utils import split_dataset_into_chunks
 from ir2.measures import eval_metrics, calc_NDCG
 import logging.config
 import sys
+from ir2.config import Config
 
 # load dataset 
 def load_dataset(dataset = "msmarco"):
@@ -49,6 +50,7 @@ def inversion_attack_loop(config):
     query_embeddings = infrence_model.get_embeddings(query_embeddings, add_gaussian_noise=False)
     score_tensor = torch.zeros((len(query_ids), len(corpus_ids)))
     batch_counter = 0
+    # maybe add extra loop for all possible noise configs?
     for batch in tqdm(split_dataset_into_chunks(corpus_ids, config.batch_size)):
         corpus_text = [corpus[id]["text"] for id in batch]
         corpus_embeddings = infrence_model.get_embeddings(
@@ -59,17 +61,17 @@ def inversion_attack_loop(config):
         )
         cosine_sim = calc_cosine_score(query_embeddings, corpus_embeddings)
         score_tensor[:, batch_counter * config.batch_size : (batch_counter+1) * config.batch_size ] = cosine_sim
-
+        batch_counter += 0
         results = infrence_model.invert_embeddings(corpus_embeddings, num_steps=config.num_steps)
         measures.append(eval_metrics(corpus_text, results))
     avg_bleu = sum([m["bleu"] for m in measures]) / len(measures)
     NDCG = calc_NDCG(score_tensor, corpus_ids, query_ids, qrels)
     
     return {"bleu" : avg_bleu, "NDCG": NDCG}
-# corpus, queries, qrels = load_dataset()
-# print(len(queries))
+
 
 if __name__ == "__main__":
+    # need to figure this part out
     config_path = sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
     config = Config.load(config_path)
     results = inversion_attack_loop(config)
