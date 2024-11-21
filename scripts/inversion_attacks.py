cdfa19119a3891d5d9c1dc374788683241ea6dfe
Plutorian-Programmer <karsten.langerak@gmail.com>
2024-11-21 10:17:37 +0100

    made lambda list based instead of one float for better loops

--- scripts/inversion_attacks.py
+++ scripts/inversion_attacks.py
@@ -51,24 +51,27 @@ def inversion_attack_loop(config):
     score_tensor = torch.zeros((len(query_ids), len(corpus_ids)))
     batch_counter = 0
     # maybe add extra loop for all possible noise configs?
-    for batch in tqdm(split_dataset_into_chunks(corpus_ids, config.batch_size)):
-        corpus_text = [corpus[id]["text"] for id in batch]
-        corpus_embeddings = infrence_model.get_embeddings(
-            corpus_text,
-            add_gaussian_noise=config.add_gaussian_noise,
-            noise_mean=config.noise_mean,
-            noise_std=config.noise_std,
-            noise_lambda=config.noise_lambda,
-        )
-        cosine_sim = calc_cosine_score(query_embeddings, corpus_embeddings)
-        score_tensor[:, batch_counter * config.batch_size : (batch_counter+1) * config.batch_size ] = cosine_sim
-        batch_counter += 0
-        results = infrence_model.invert_embeddings(corpus_embeddings, num_steps=config.num_steps)
-        measures.append(eval_metrics(corpus_text, results))
-    avg_bleu = sum([m["bleu"] for m in measures]) / len(measures)
-    NDCG = calc_NDCG(score_tensor, corpus_ids, query_ids, qrels)
-    
-    return {"bleu" : avg_bleu, "NDCG": NDCG}
+    lambda_list = config.noise_lambda_list
+    result_dict = dict()
+    for noise_lambda in lambda_list:
+        for batch in tqdm(split_dataset_into_chunks(corpus_ids, config.batch_size)):
+            corpus_text = [corpus[id]["text"] for id in batch]
+            corpus_embeddings = infrence_model.get_embeddings(
+                corpus_text,
+                add_gaussian_noise=config.add_gaussian_noise,
+                noise_mean=config.noise_mean,
+                noise_std=config.noise_std,
+                noise_lambda = noise_lambda,
+            )
+            cosine_sim = calc_cosine_score(query_embeddings, corpus_embeddings)
+            score_tensor[:, batch_counter * config.batch_size : (batch_counter+1) * config.batch_size ] = cosine_sim
+            batch_counter += 0
+            results = infrence_model.invert_embeddings(corpus_embeddings, num_steps=config.num_steps)
+            measures.append(eval_metrics(corpus_text, results))
+        avg_bleu = sum([m["bleu"] for m in measures]) / len(measures)
+        NDCG = calc_NDCG(score_tensor, corpus_ids, query_ids, qrels)
+        result_dict[noise_lambda] = {"bleu" : avg_bleu, "NDCG": NDCG}
+    return result_dict
 
 
 if __name__ == "__main__":
